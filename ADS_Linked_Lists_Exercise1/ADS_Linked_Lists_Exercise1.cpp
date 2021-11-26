da26cadf4341e6dc3664fcac3729a3e095d43681
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-11-26 10:59:15 +0000

    Added exception on Clear

--- ADS_Linked_Lists_Exercise1/ADS_Linked_Lists_Exercise1.cpp
+++ ADS_Linked_Lists_Exercise1/ADS_Linked_Lists_Exercise1.cpp
@@ -43,17 +43,55 @@ void simpleLinkedListSizeDemo() {
 	SimpleNode<int> n3(30);
 	SimpleNode<int> n4(40);
 
+	intList.add(&n1); //pointerize an object by using & operator -> return address of object
+	intList.add(&n2); //pointerize an object by using & operator -> return address of object
+	intList.add(&n3); //pointerize an object by using & operator -> return address of object
+	intList.add(&n4); //pointerize an object by using & operator -> return address of object
+
 	//test size
+	cout << intList.count() << endl;
+}
+
+bool isOdd(int x) {
+	return x % 2 == 1;
+}
+
+void simpleLinkedListSearchDemo() {
+	//make list
+	SimpleLinkedList<int> intList;
+
+	//make some nodes
+	SimpleNode<int> n1(12);
+	SimpleNode<int> n2(22);
+	SimpleNode<int> n3(34);
+	SimpleNode<int> n4(41);
+
+	intList.add(&n1); //pointerize an object by using & operator -> return address of object
+	intList.add(&n2); //pointerize an object by using & operator -> return address of object
+	intList.add(&n3); //pointerize an object by using & operator -> return address of object
+	intList.add(&n4); //pointerize an object by using & operator -> return address of object
+
+	//test size
+
+	//use a hard-coded function
+	cout << intList.search(isOdd) << endl;
+
+	//use a lambda expression
+	auto isEven = [](int value) { return value % 2 == 0; };
+	cout << intList.search(isEven) << endl;
 }
 
 int main()
 {
 	//cout << "simpleNodeDemo..." << endl;
-//	simpleNodeDemo();
+	//simpleNodeDemo();
 
-	cout << "simpleLinkedListDemo..." << endl;
-	simpleLinkedListDemo();
+	//cout << "simpleLinkedListDemo..." << endl;
+	//simpleLinkedListDemo();
 
-	cout << "simpleLinkedListSizeDemo..." << endl;
-	simpleLinkedListSizeDemo();
+	//cout << "simpleLinkedListSizeDemo..." << endl;
+	//simpleLinkedListSizeDemo();
+
+	cout << "simpleLinkedListSearchDemo..." << endl;
+	simpleLinkedListSearchDemo();
 }
--- ADS_Linked_Lists_Exercise1/SimpleNode.h
+++ ADS_Linked_Lists_Exercise1/SimpleNode.h
@@ -9,7 +9,6 @@ using namespace std;
 2. size, clear
 3. search
 4. predicate
-
 */
 
 template <typename E>
@@ -22,6 +21,10 @@ public:
 	SimpleNode(E data) {
 		this->data = data;
 		pNext = nullptr;
+	}
+
+	E getData() const {
+		return this->data;
 	}
 
 	SimpleNode* getNext() const {
@@ -41,16 +44,25 @@ template <typename E>
 class SimpleLinkedList {
 private:
 	SimpleNode<E>* pHead;
+	int size;
 
 public:
 	SimpleLinkedList() {
 		this->pHead = nullptr;
+		this->size = 0;
 	}
 
-	//template typename<E>
+	~SimpleLinkedList() {
+		//TODO - clear?
+	}
+
 	void add(SimpleNode<E>* pNode) {
-		if (this->pHead == nullptr)
+		if (pNode == nullptr)
+			throw invalid_argument("New node data cannot be null!");
+
+		if (this->pHead == nullptr) {
 			pHead = pNode;
+		}
 		else
 		{
 			SimpleNode<E>* pCurrent = pHead; //Node(10)
@@ -65,21 +77,47 @@ public:
 			}
 			pCurrent->setNext(pNode);
 		}
+
+		this->size++;
 	}
 
-	int size()
+	int count()
 	{
+		return this->size;
 	}
 
 	int search(E target)
 	{
+		auto pred = [target](int value) { return target == value; };
+		return search(pred);
 	}
 
-	int search(function<bool(E)> predicate) {
+	int search(function<bool(E)> predicate)
+	{
+		//store position
+		int index = 0;
+		//iterate through each node
+		SimpleNode<E>* pNext = pHead;
+
+		//until we hit the tail
+		while (pNext != nullptr) {
+			//test the predicate
+			if (predicate(pNext->getData()))
+				return index;
+
+			//continue with next
+			pNext = pNext->getNext();
+
+			//increment index
+			index++;
+		}
+
+		return -1;
 	}
 
 	void clear()
 	{
+		throw "Implement me!";
 	}
 
 	void print() const {
